bd1bb9b68cfb531ce4552752f8a92e8de5805869
Elianis Medina <elianismedina05@outlook.com>
2023-05-24 23:51:41 -0500

    fixed

--- src/main/java/com/example/demo/Reto3bApplication.java
+++ src/main/java/com/example/demo/Reto3bApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Reto3bApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Reto3bApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Reto3bApplication.class, args);
+    }
 
 }
