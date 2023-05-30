b9d88cfe49a84ec6e836243ce37257dcc0b905b4
Elianis Medina <elianismedina05@outlook.com>
2023-05-30 16:21:59 -0500

    fixed adapter

--- src/main/java/com/example/demo/Security/SecurityAdapter.java
+++ src/main/java/com/example/demo/Security/SecurityAdapter.java
@@ -13,7 +13,7 @@ public class SecurityAdapter extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests(
-                a -> a.antMatchers("/","error","/webjars/**","/api/**").permitAll().anyRequest().authenticated()
+                a -> a.antMatchers("/","/error","/webjars/**","/api/**").permitAll().anyRequest().authenticated()
         ).exceptionHandling(
                 e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.FORBIDDEN))
         ).oauth2Login().defaultSuccessUrl("/static/homePage.html", true);
