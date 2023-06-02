3ab7ed7c02cc0c4d7c43a09438518467eb6b4d3f
Elianis Medina <elianismedina05@outlook.com>
2023-06-01 20:04:06 -0500

    added dtos

--- src/main/java/com/example/demo/Repositorio/CRUD/ReservationCrudRepository.java
+++ src/main/java/com/example/demo/Repositorio/CRUD/ReservationCrudRepository.java
@@ -5,12 +5,23 @@
 package com.example.demo.Repositorio.CRUD;
 
 import com.example.demo.Modelo.Reservation;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  *
  * @author USUARIO
  */
 public interface ReservationCrudRepository extends CrudRepository<Reservation, Integer>{
+
+    public List<Reservation> findAllByStartDateAfterAndDevolutionDateBefore(Date fechaA, Date fechaB);
+    public List<Reservation> findAllByStatus(String status);
+    @Query("SELECT c.client, COUNT(c.client) FROM Reservation AS c GROUP BY c.client ORDER BY COUNT(c.client) DESC")
+    public List<Object[]> getTotalReservationsByClient();
+
+
     
 }
--- src/main/java/com/example/demo/Repositorio/ReservationRepository.java
+++ src/main/java/com/example/demo/Repositorio/ReservationRepository.java
@@ -4,12 +4,17 @@
  */
 package com.example.demo.Repositorio;
 
+import com.example.demo.Modelo.Client;
+import com.example.demo.Modelo.DTOs.TotalAndClient;
 import com.example.demo.Modelo.Reservation;
 import com.example.demo.Repositorio.CRUD.ReservationCrudRepository;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 /**
  *
@@ -43,6 +48,26 @@ public class ReservationRepository {
     
         reservationCrudRepository.delete(reservation);
     }
+    public List<Reservation> getReservationsBetweenDates(Date fechaA,Date fechaB){
+        return reservationCrudRepository.findAllByStartDateAfterAndDevolutionDateBefore(fechaA,fechaB);
+    }
+
+    public  List<Reservation> getReservationsByStatus(String status){
+        return reservationCrudRepository.findAllByStatus(status);
+    }
+
+    public List<TotalAndClient> getTopClients(){
+        List<TotalAndClient> respuesta = new ArrayList<>();
+        List<Object[]> reporte = reservationCrudRepository.getTotalReservationsByClient();
+
+        for (Object[] pareja: reporte
+             ) {
+            respuesta.add(new TotalAndClient((Long) pareja[1],(Client) pareja[0]));
+            
+        }
+        return respuesta;
+    }
+
     
 }
 
